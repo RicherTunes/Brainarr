2461a9379d4d1bdc366701e951884de40472274c
RicherTunes <xfear26@hotmail.com>
2025-09-14 17:58:36 -0400

    ci: unit-only PR gate + add sanity-build workflow (Docker assemblies + fast build)

--- Brainarr.Plugin/Services/Support/VoidResult.cs
+++
@@ -1,9 +0,0 @@
-namespace NzbDrone.Core.ImportLists.Brainarr.Services
-{
-    // Helper class for void async operations with RateLimiter
-    public class VoidResult
-    {
-        public static readonly VoidResult Instance = new VoidResult();
-        private VoidResult() { }
-    }
-}
